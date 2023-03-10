d7986c0d1c65b6fb4afc12570a5fe3011c61abc8
shamim <shamimice03@gmail.com>
2023-03-10 00:52:22 +0000

    Initial Commit

---
+++ versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.57.1"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.4.3"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "terraform-user"
+}
